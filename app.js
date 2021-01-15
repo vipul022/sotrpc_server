02219af67857fd6d8c4bd9ff460d1ed8147a480c
drumovski <jason.stacy@gmail.com>
2021-01-15 17:10:35 +1100

    remove samesite strict

--- app.js
+++ app.js
@@ -35,7 +35,6 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
       expires: 3600000,
-      sameSite: 'strict'
     },
     store: new MongoStore({
       mongooseConnection: mongoose.connection,
